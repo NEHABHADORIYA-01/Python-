975190f4d3f352e4ad7a21344dac5de62405292e
NEHA BHADORIYA-01 <bhadoriyaneha55@gmail.com>
2024-12-20 12:53:19 +0530

    Addition of three numbers

---
+++ Addition.py
@@ -0,0 +1,7 @@
+#Sum of three digits 
+Num_1 = float(input("Enter First Number: "))
+Num_2 = float(input("Enter Second Number: "))
+Num_3 = float(input("Enter Third Number: "))
+Result = Num_1 + Num_2 + Num_3
+print("Sum of these three number is : ", Result)
+
